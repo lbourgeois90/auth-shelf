b686f56febe562b8a7a0472e667b6fcc96f474f0
Lili Bourgeois <lbourgeois90@gmail.com>
2019-04-18 14:29:49 -0500

    added input fields and functions for handle submit and change

---
+++ src/components/AddItem/AddItem.js
@@ -0,0 +1,63 @@
+import React, { Component, Fragment } from 'react';
+import {connect} from 'react-redux';
+
+class AddItem extends Component {
+    
+    state ={
+        newItem:{
+            description: '',
+            image_url:'',
+        }
+    }
+
+    handleChangeFor = (propertyName) => {
+        return (event) => {
+          this.setState({
+            newItem:  {
+              ...this.state.newItem,
+              [propertyName]: event.target.value,
+            }
+          })
+        }
+      }
+
+      handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.dispatch({type: 'ADD_ITEM', payload: this.state.newItem})
+        this.setState({
+            newItem:{
+              description: '',
+              image_url:'',
+          },
+        })
+      }
+    
+      
+  // Renders the entire app on the DOM
+  render() {
+    return (
+    <section>
+        <h2>Add New Items</h2>
+        <form>
+        <label>Item Description:</label>
+        <input type="text" 
+               value={this.state.newItem.description}
+               onChange={this.handleChangeFor('description')} />
+        <br />
+        <label>From:</label>
+        <input type="text" 
+               value={this.state.newItem.image_url}
+               onChange={this.handleChangeFor('image_url')} />
+        <button onClick={this.handleSubmit}>Add</button>
+        </form>
+    </section>
+    )
+  }
+}
+
+
+const mapReduxStateToProps = (reduxState) => ({
+    reduxState,
+})
+
+export default connect(mapReduxStateToProps)(AddItem);
